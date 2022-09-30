d88040b267052d83fc8bd184cc3e0036cf7c8f37
TheHarryCode <theharrycode@gmail.com>
2022-09-29 20:24:56 -0500

    Continuidad Curso de SpringBoot

--- src/main/java/com/thc/curso/springboot/controller/CursoSpringController.java
+++ src/main/java/com/thc/curso/springboot/controller/CursoSpringController.java
@@ -1,5 +1,7 @@
 package com.thc.curso.springboot.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.thc.curso.springboot.model.MarcaDTO;
 import com.thc.curso.springboot.model.RequestMarcas;
@@ -28,16 +32,31 @@ public class CursoSpringController {
 
 	@Autowired
 	ICursoSpringService service;
-	
+
 	@Value("${spring.datasource.url}")
 	private String url;
+
+	@PostMapping("/uploadFile")
+	@ResponseStatus(HttpStatus.CREATED)
+	public MarcaDTO uploadFile(@RequestParam("file") MultipartFile file) {
+		return MarcaDTO.builder().idMarca(100L).nombreMarca("You successfully uploaded ").build();
+	}
 	
 	//GET --> Obtener
 	@GetMapping("/getMarcas")
 	@ResponseStatus(HttpStatus.OK)
 	public List<MarcaDTO> getMarcas(@RequestHeader("Autorizador") String token){
-		System.out.println("URL DE la BD "+url);
 		return service.getMarcas(token);
+	}
+
+	@GetMapping("/getMarcasAll")
+	@ResponseStatus(HttpStatus.OK)
+	public List<MarcaDTO> getMarcasAll(){
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		System.out.println("Petición a las "+dtf.format(LocalDateTime.now()));
+
+		return service.getMarcas("");
 	}
 
 	@GetMapping("/getMarcaxId/{idMarca}")
